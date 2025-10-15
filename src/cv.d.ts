0b80ed623c2455095ff381e5b229e25d13c57f17
Jose Aguilar <joshxyev@gmail.com>
2025-10-15 12:57:37 +0200

    wip: WIP

--- src/cv.d.ts
+++ src/cv.d.ts
@@ -1,95 +1,93 @@
 export interface CV {
-  basics: Basics
-  work: Array<Work>
-  volunteer: Array<Volunteer>
-  education: Array<Education>
-  awards: Array<Awards>
-  certificates: Array<Certificates>
-  publications: Array<Publications>
-  skills: Array<Skills>
-  languages: Array<Languages>
-  interests: Array<Interests>
-  references: Array<References>
-  projects: Array<Projects>
+  basics: Basics;
+  work: Array<Work>;
+  volunteer: Array<Volunteer>;
+  education: Array<Education>;
+  awards: Array<Awards>;
+  certificates: Array<Certificates>;
+  publications: Array<Publications>;
+  skills: Array<Skills>;
+  languages: Array<Languages>;
+  interests: Array<Interests>;
+  references: Array<References>;
+  projects: Array<Projects>;
 }
 
 interface Basics {
-  name: string
-  label: string
-  image: string
-  email: string
-  phone: string
-  url: string
-  summary: string
-  location: Location
-  profiles: Array<Profiles>
+  name: string;
+  label: string;
+  image: string;
+  email: string;
+  phone: string;
+  url: string;
+  summary: string;
+  location: Location;
+  profiles: Array<Profiles>;
 }
 
 interface Location {
-  address: string
-  postalCode: string
-  city: string
-  countryCode: string
-  region: string
+  address: string;
+  postalCode: string;
+  city: string;
+  countryCode: string;
+  region: string;
 }
 
 interface Profiles {
-  network: string
-  username: string
-  url: string
+  network: string;
+  username: string;
+  url: string;
 }
 
 interface Work {
-  name: string
-  position: string
-  url: string
-  startDate: DateStr
-  endDate: DateStr | null
-  summary: string
-  highlights: Highlight
+  name: string;
+  position: string;
+  url: string;
+  startDate: DateStr;
+  endDate: DateStr | null;
+  summaries: string[];
+  highlights: Highlight;
 }
 
-type DateStr = `${string}-${string}-${string}`
-
+type DateStr = `${string}-${string}-${string}`;
 
 interface Skills {
-  name: string
+  name: string;
 }
 
-
 interface Certificates {
-  name: string,
-  date: DateStr,
-  issuer: string,
-  url: string
+  name: string;
+  date: DateStr;
+  issuer: string;
+  url: string;
 }
 
 interface Publications {
-  name: string
-  publisher: string
-  releaseDate: DateStr
-  url: string
-  summary: string
+  name: string;
+  publisher: string;
+  releaseDate: DateStr;
+  url: string;
+  summary: string;
 }
 
 interface Education {
-  institution: string
-  url: string
-  area: string
-  studyType: string
-  startDate: DateStr
-  endDate: DateStr
-  score: string
-  courses: Array<string>
+  institution: string;
+  url: string;
+  area: string;
+  studyType: string;
+  startDate: DateStr;
+  endDate: DateStr;
+  score: string;
+  courses: Array<string>;
 }
 
 interface Languages {
-  language: Language
-  fluency: string
+  language: Language;
+  fluency: string;
 }
 
 type Language =
-  "Spanish"
+  | "Spanish"
   | "English"
   | "German"
   | "France"
@@ -104,8 +102,6 @@ type Language =
   | "Turkish"
   | "Hindi"
   | "Bengali"
-  | string
+  | string;
 
-
-
-type Highlight = Array<String>
+type Highlight = Array<String>;
